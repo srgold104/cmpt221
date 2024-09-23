d0dbfbc0c3cb42274a08455741bcf848cd45aefb
srgold104 <seth.goldstein1@marist.edu>
2024-09-23 09:50:56 -0400

    updated lab-3 with correct ERD

--- labs/lab-3/db/schema/professor.py
+++ labs/lab-3/db/schema/professor.py
@@ -4,17 +4,23 @@ from db.db import db
 class Professor(db.Model):
     __tablename__ = 'Professors'
     ProfessorID = db.Column(db.Integer,primary_key=True, autoincrement=True)
+    FirstName = db. Column (db. String (40))
+    LastName = db. Column (db. String (40))
+    Email = db. Column (db. String (40))
 
     # create relationship with courses table. assoc table name = ProfessorCourse
     course = db.relationship('Courses', secondary = 'ProfessorCourse', back_populates = 'Professors')
     def __init__(self):
-        # remove pass and then initialize attributes
-        pass
+        self.FirstName = self.FirstName
+        self.LastName = self.LastName
+        self.Email = self.Email
 
     def __repr__(self):
         # add text to the f-string
         return f"""
-
+            "Professor's First Name: {self.FirstName},
+            Professor's Last Name: {self.LastName},
+            Professor's Email: {self.Email}
         """
     
     def __repr__(self):
